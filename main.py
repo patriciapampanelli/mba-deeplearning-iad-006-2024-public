47fb39015b406a1fe495ff68ecf88e96a2d17430
Debs <64430260+dehlogin@users.noreply.github.com>
2024-08-20 20:27:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI, File
+from pydantic import BaseModel
+import xgboost as xgb
+import numpy as np
+import pickle
+import warnings
 
+import base64
+from PIL import Image
+import io
+
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
+
+app = FastAPI()
+
+#Definição dos tipos de dados
+class PredictionResponse(BaseModel):
+  prediction: float
+
+class Imagerequest(BaseModel):
+  image: str
